b0e8506d4e775af2aa94bedff10dd915d00af5c0
durga750 <78526756+durga750@users.noreply.github.com>
2021-02-09 15:48:00 +0530

    Add files via upload

---
+++ Isperfect.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserDefinedMethods
+{
+    class Isperfect
+    {
+        public static int Perfect(int num)
+        {
+            int sum = 0;
+            int temp = num;
+            for (int i = 1; i < num; i++)
+            {
+                if (num % i == 0)
+                {
+                    sum += i;
+                }
+            }
+            return sum;
+        }
+
+        public static bool IsPerfectNum(int num)
+        {
+            int temp = num;
+            return Perfect(num) == temp;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(IsPerfectNum(6));
+
+        }
+    }
+}
